806f6e8068b7ff523b6965c94e446fc94a9da3a4
Alex Diaz <alextauresdiaz@gmail.com>
2023-02-28 13:24:05 -0600

    correction to route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Routes, NavLink} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes, NavLink, Link, Navigate} from 'react-router-dom'
 import Home from './components/Home';
 import Projects from './components/Projects';
 import Formation from './components/Formation';
@@ -11,8 +11,8 @@ function App() {
    <div class="context">
    <Router>
       <div className='navbar'>
-          <NavLink to='/' className='navlink'><i class="fa-sharp fa-solid fa-house-chimney"></i></NavLink>
-          <NavLink to='/projects' className='navlink'>Projects</NavLink>
+          <Link to='/' className='navlink'><i class="fa-sharp fa-solid fa-house-chimney"></i></Link>
+          <Link to='/projects' className='navlink'>Projects</Link>
           <a href='http://linkedin.com/in/alex-taures' target='_blank' rel='noreferrer' className='externalLink'><i class="fa-brands fa-linkedin"></i>Linkedin</a>
           <a href='https://github.com/AlexTaures' target='_blank' rel='noreferrer' className='externalLink'><i class="fa-brands fa-github"></i>GitHub</a>
       </div>
@@ -23,9 +23,12 @@ function App() {
           }/>
 
 
-      <Route exact path="/" element={
-            <Home/>
+      <Route path="/" element={
+            <Home/> 
           }/>
+      
+      <Route path="*" element={<Navigate to="/" replace={true} />}
+    />
 
       </Routes>
    </Router>
